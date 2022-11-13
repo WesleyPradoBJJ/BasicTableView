c349fef55cecbcc601827ddd2b6d53923705c675
Wesley Prado <wesleypradobjj@gmail.com>
2022-11-13 05:39:35 +0400

    Starting the project.

---
+++ BasicTableView/BasicTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicTableView
+//
+//  Created by Wesley Prado on 13/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
